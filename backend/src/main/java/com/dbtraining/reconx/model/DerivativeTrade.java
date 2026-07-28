a1831e4458f4f0118fdc71cdc7239e5e98af7a0b
Rakshita Agrawal <rakshitaagrawal2003@gmail.com>
2026-07-28 11:15:35 +0530

    Implement equals and hashCode for ADV028

--- backend/src/main/java/com/dbtraining/reconx/model/DerivativeTrade.java
+++ backend/src/main/java/com/dbtraining/reconx/model/DerivativeTrade.java
@@ -63,14 +63,16 @@ public final class DerivativeTrade implements TradeType {
     public Side side()               { return side; }
     public long counterpartyId()     { return counterpartyId; }
 
-    @Override public boolean equals(Object o) {
-        // TODO(TICKET-ADV028): pattern-match on DerivativeTrade and compare tradeRef.
-        throw new UnsupportedOperationException("TICKET-ADV028");
-    }
-    @Override public int hashCode() {
-        // TODO(TICKET-ADV028): hash from tradeRef.
-        throw new UnsupportedOperationException("TICKET-ADV028");
-    }
+    @Override
+public boolean equals(Object o) {
+    return (o instanceof DerivativeTrade other)
+            && tradeRef.equals(other.tradeRef);
+}
+
+@Override
+public int hashCode() {
+    return tradeRef.hashCode();
+}
 
     @Override public String toString() {
         // TODO(TICKET-ADV030): "DerivativeTrade[ref=..., TYPE UNDERLYING on date, strike=... CCY, qty=..., expiry=..., side=...]"
